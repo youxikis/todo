62f64623fcc89ceb2ba4af3800938daddfe51ec0
youxikis <youxikis@163.com>
2016-11-02 23:16:32 +0800

    gai

--- js/index2.js
+++ js/index2.js
@@ -11,23 +11,22 @@
 	var startPos,endPos;
 	var del=$(".todos li .del")
 	var clearAll=$(".clearAll")
-//  var todo;
  if(localStorage.x)
-   {
+   {  
    	  dodos=JSON.parse(localStorage.x);
       for(var i=0;i<dodos.length;i++)
-   	  
    	  {
-// 	  	 var c=(dos[i].state)?'done':'on';
-// 	  	 $("<li class="+c+"><div class='content'>"+dos[i].name+"</div><div class='delete'></div></li>").appendTo(todos);
-   	  	 $("<li class=''><div class='content'>"+dodos[i].name+"</div><div class='del'>×</div></li>").appendTo(todos);
+
+   	  	 $("<li class='addli'><textarea class='content' disabled>"+dodos[i].name+"</textarea><div class='del'>×</div></li>").appendTo(todos);
    	  }
    }
 
 	//添加
 	add.on("touchend",function(){
-        box.addClass("dis");
+		todos.css("display","none");
+        box.css("display","block");
 	})
+	//点击编辑进行编辑
 	write.on("touchend",function(){
 		inCon.removeAttr("disabled");
 		
@@ -37,56 +36,75 @@
 		confirm.css("display","block");
 		
 	})
+	//点击√确认添加
 	confirm.on("touchend",function(){
 		var v=inCon.val();
 		var todo={
 	   	 	name:v,
 	   	    state:0
 	   	 };
-		var li=$("<li><div class='content' >"+todo.name+"</div><div class='del'>×</div></li>");
+		var li=$("<li class='addli'><textarea class='content' disabled >"+todo.name+"</textarea><div class='del'>×</div></li>");
 		con.val(todo.name)
 	    li.appendTo(todos);
-	    console.log(todo)
 	    dodos.push(todo);
 	    localStorage.x=JSON.stringify(dodos);
-	    box.addClass("disnone");
+	    box.css("display","none");
+	    todos.css("display","block");
+	    confirm.css("display","none")
+	    write.removeClass("disnone");
+	    inCon.val("");
+	    inCon.attr("disabled","disabled");
 	})
 		
 //删除
-
-$(".todos li").on("touchend",".del",function(){
-	 var li=$(this).closest("li");
-	 var index=li.index();
-	 dodos.splice(index,1);
-	 localStorage.x=JSON.stringify(dodos);
-	 $(this).closest("li").remove();
-})
-$(".todos li ").on("touchstart",function(e){
-	startPos=e.originalEvent.changedTouches[0].clientX;
-})
-$(".todos li ").on("touchend",function(e){
-	endPos=e.originalEvent.changedTouches[0].clientX;
-	
-	if(endPos-startPos>50){
-		$(this).children(".del").css("display","block");
-	}
-	if(endPos-startPos<-50){
-		$(this).children(".del").css("display","none");
-	}
-})
+	$(".todos ").on("touchend","li .del",function(){
+		 var li=$(this).closest("li");
+		 var index=li.index();
+		 dodos.splice(index,1);
+		 localStorage.x=JSON.stringify(dodos);
+		 $(this).closest("li").remove();
+	})
+	$(".todos").on("touchstart", "li",function(e){
+		startPos=e.originalEvent.changedTouches[0].clientX;
+	})
+	$(".todos").on("touchend","li",function(e){
+		endPos=e.originalEvent.changedTouches[0].clientX;
+		if(endPos-startPos>50){
+			console.log(6)
+			$(this).children(".del").css("display","block");
+			$(this).children(".content").addClass("done")
+		}
+		if(endPos-startPos<-50){
+			$(this).children(".del").css("display","none");
+			$(this).children(".content").removeClass("done")
+		}
+	})
 
 
 //删除全部
-clearAll.on("touchend",function(){
-	var newarr=[];
-   for(var i=0;i<todos.length;i++)
-   {
-   	 	newarr.push(todos[i]);
-   	 
-   }
-   localStorage.x=JSON.stringify(newarr);
-   	$(".todos li ").remove();
-})
+	clearAll.on("touchend",function(){
+	   localStorage.x="";
+		$(".todos").remove();
+	})
+
+
+//修改
+/*	var addli=$(".addli");
+	addli.each(function(i){
+		addli.eq(i).on("touchend",function(){
+//			var val=$(this).children("textarea").val();
+			box.css("display","block");
+			todos.css("display","none");
+//			inCon.val(val);
+			console.log(inCon.val())
+			confirm.on("touchend",function(){
+				
+				$(this).children("textarea").val(inCon.val());
+			})
+		})
+		
+	})*/
+	
 
  })
  
